71a2e89e097745e7dc1280c3d0354a2ba32382b5
New Panuwat <new_sbh@me.com>
2022-08-06 21:31:10 +0700

    tag deploy v0.1.0

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,7 @@
 #!/bin/sh
 
+git pull --rebase
 git add .
-msg="update readme site"
-
-if [ $# -eq 1 ]; then msg="$1"; fi
-
-git commit -m "$msg"; git pull --rebase; git push origin main
-
-# tag deploy
-# git pull --rebase
-# git commit -m "tag deploy v0.1.0"
-# git tag -a v0.1.0 -m "update tag deploy 0.1.0"
-# git push origin v0.1.0
+git commit -m "tag deploy v0.1.0"
+git tag -a v0.1.0 -m "update tag deploy 0.1.0"
+git push origin main
